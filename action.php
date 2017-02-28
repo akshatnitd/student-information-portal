06efed9efc2122a87249803be0a258045114698d
Akshat Garg <coolakshat24@gmail.com>
2017-03-01 00:04:29 +0530

    Add action script on login button submit

---
+++ action.php
@@ -0,0 +1,68 @@
+<?php
+ 
+// include('connection.php');
+define('HOST','localhost');
+define('USER','root');
+define('PASS','');
+define('DB','student-information-portal');
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $email = test_input($_POST["email"]);
+    $pass = test_input($_POST["pass"]);
+
+    $con = mysqli_connect(HOST,USER,PASS,DB);
+
+    //$sql  = "SELECT * FROM users";
+    $sql = "SELECT * FROM users WHERE  email='".$email."' and password='".$pass."' ";
+
+    $res = mysqli_query($con,$sql);
+     
+    $result = array();
+    
+    if(isset($res))
+    {
+        while($row = mysqli_fetch_array($res)){
+        array_push($result,
+        array('fname'=>$row[0],
+        'lname'=>$row[1],
+        'gender'=>$row[2],
+        'dob'=>$row[3],
+        'addr'=>$row[4],
+        'roll'=>$row[5],
+        'prog'=>$row[6],
+        'dept'=>$row[7],
+        'sem'=>$row[8]
+        ));
+    }
+
+        if (empty($result)) {
+            echo "Failed\n";            
+        }
+        else
+        {
+            echo "success\n";
+            header('Location: success.html');   
+            //echo json_encode(array("result"=>$result));    
+        }
+        mysqli_close($con);
+    }
+    else
+    {
+        echo "Failed\n";
+    }
+    
+ 
+    
+}
+
+?>
+
+
