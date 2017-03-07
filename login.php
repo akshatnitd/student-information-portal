1b7db3a507b06220794cd1bebe78ed870b20c9c0
Akshat Garg <coolakshat24@gmail.com>
2017-03-07 21:06:05 +0530

    Add navbar to index page

--- login.php
+++ login.php
@@ -7,13 +7,75 @@
     href = "/css/main.css" />
 </head>
 <body>
+<?php
+ 
+define('HOST','localhost');
+define('USER','root');
+define('PASS','labwebsite');
+define('DB','student_information_portal');
 
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+    $email = test_input($_POST["email"]);
+    $pass = test_input($_POST["pass"]);
+
+    $con = mysqli_connect(HOST,USER,PASS,DB);
+
+    $sql = "SELECT * FROM users WHERE  email='".$email."' and password='".$pass."' ";
+
+    $res = mysqli_query($con,$sql);
+     
+    $result = array();
+    
+    if(isset($res))
+    {
+        while($row = mysqli_fetch_array($res)){
+            array_push($result,
+            array('fname'=>$row[0],
+            'lname'=>$row[1],
+            'gender'=>$row[2],
+            'dob'=>$row[3],
+            'addr'=>$row[4],
+            'roll'=>$row[5],
+            'prog'=>$row[6],
+            'dept'=>$row[7],
+            'sem'=>$row[8]
+            ));
+        }
+
+        if (empty($result)) {
+            //$('div#err').html('Incorrect credentials');
+            //echo "Failed\n";            
+        }
+        else
+        {
+            echo "success\n";
+            header('Location: home.html');   
+            //echo json_encode(array("result"=>$result));    
+        }
+
+        mysqli_close($con);
+    }
+    else
+    {
+        echo "Failed conn\n";
+    }
+    
+}
+?>
     <header
         <h2 class="fs-title" style="text-align: center; color: #fff; font-size: 24px; padding-top: 50px;">Login Page</h2>
     </header>
 
-    <form id="msform" method="POST" action='action.php' onsubmit="return loginsubmit();">
+    <form id="msform" method="POST" action='<?php echo $_SERVER['PHP_SELF']; ?>' onsubmit="return loginsubmit();">
         <fieldset>
             <h2 class="fs-title">Enter your login details</h2>
             <br />
